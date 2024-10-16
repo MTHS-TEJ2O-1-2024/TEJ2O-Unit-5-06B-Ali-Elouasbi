c54f326dcc6c48caf108628ebb1a06a9615b8210
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-10-16 11:22:06 -0400

    5-06

--- main.ts
+++ main.ts
@@ -5,4 +5,19 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let distanceToObject:number = 0
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+//find distance from sonar
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
